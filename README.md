d9677e8160817334a18488877837302800525b8c
Student <student@Makerss-MacBook-Pro.local>
2019-09-02 13:08:54 +0100

    Implemented the points system

--- README.md
+++ README.md
@@ -1,13 +1,15 @@
 ## Rock Paper Scissors - JavaScript
 
 I am trying trough this project to refresh my knowledge of Vanilla Javascript, by doing something fun.
- The idea of the game is to have an interactive web page in which the user will play 'Rock Paper Scissors' against a computer, by clicking one of the 3 given options, the computer will select a random option as well; both results will be compared; a message will be displayed (if you lose or win) and a form of score will be kept. I will try later on to addapt it for player vs player, by hosting it on a web page and connecting 2 players to play remotely.
+ The idea of the game is to have an interactive web page in which the user will play 'Rock Paper Scissors' against a computer, by clicking one of the 3 given options, the computer will select a random option as well; both results will be compared; a message will be displayed (if you lose or win)and a form of score will be kept. 
+   I will try later on to addapt it for player vs player, by hosting it on a web page and connecting 2 
+players to play remotely.
 #### HTML elements and CSS styling
 
 
 I started by creating the Html elements needed. I found and downloaded the appropriate icons (png files) for the Rock, Paper, Scissors. Then I have used CSS to style all the Html elements.
 
-#### JavaScriot
+#### JavaScript
  
  I, initially, declared and assigned, without Jquery, all the element classes and ids from the index page. 
  Afterword I have created 3 methods, for each buttopn to test with 'console.log' if clicking the buttons will work, 
--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-const userScor = 0;
-const computerScore = 0;
+var userScore = 0;
+var computerScore = 0;
 const userScore_span = document.getElementById("user-score");
 const computerScore_span = document.getElementById("computer-score")
 const scoreBoard_div = document.querySelector(".score-board");
-const result_div = document.querySelector(".result");
+const result_div_p = document.querySelector(".result > p");
 const rock_div = document.getElementById("r");
 const paper_div = document.getElementById("p");
 const scissors_div = document.getElementById("s");
@@ -14,23 +14,47 @@ function getComputerChoice() {
   return choices[randomComputerChoice];
 }
 
+function changeNames(letter) {
+  if(letter === 'r'){ return "Rock"}
+  else if(letter === 'p'){ return "Paper"}
+  else if(letter === 's'){ return "Scissors"}
+}
+
+function win(userChoice, computerChoice){
+  userScore++;
+  userScore_span.innerHTML = userScore;
+  computerScore_span.innerHTML = computerScore;
+  result_div_p.innerHTML = changeNames(userChoice) + " beats " + changeNames(computerChoice) + ". You win!";
+}
+
+function lose(userChoice, computerChoice){
+  computerScore++;
+  userScore_span.innerHTML = userScore;
+  computerScore_span.innerHTML = computerScore;
+  result_div_p.innerHTML = changeNames(userChoice) + " is beaten by " + changeNames(computerChoice) + ". You lose!";
+}
+
+function draw(userChoice, computerChoice){
+  result_div_p.innerHTML = changeNames(userChoice) + " is equal with " + changeNames(computerChoice) + ". It's a Draw!";
+}
+
 function game(userChoice) {
   const computerChoice = getComputerChoice();
   switch(userChoice + computerChoice) {
     case 'rs':
     case 'sp':
     case 'pr':
-      console.log("You win!");
-      break;
+     win(userChoice, computerChoice);
+     break;
     case 'rp':
     case 'ps':
     case 'sr':
-      console.log("You lose!");
+      lose(userChoice, computerChoice);
       break;
     case 'rr':
     case 'ss':
     case 'pp':
-      console.log("Draw!");
+      draw(userChoice, computerChoice);
       break;
   }
 }
